57f6775e8f77b83218af7d451ffc7588a9f6a239
Mansourthior <2mt331@gmail.com>
2020-12-26 14:32:54 +0000

    first

---
+++ src/main/java/com/hemti/nopal/NopalApplication.java
@@ -0,0 +1,13 @@
+package com.hemti.nopal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NopalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NopalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hemti/nopal/NopalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemti.nopal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NopalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
